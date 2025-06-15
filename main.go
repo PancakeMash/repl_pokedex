de374735f5097d0d36e1bc706543922b07bafc6c
PancakeMash <arpandevofficial@gmail.com>
2025-06-15 14:37:20 +0100

    D: Fixed the pokedex CLI to continuously loop through regardless of input and added in callback functions.

--- main.go
+++ main.go
@@ -7,12 +7,39 @@ import (
 	"strings"
 )
 
+// Init the cliCommand at the package level so that callback functions can access it.
+var cli_cmd map[string]cliCommand
+
 func main() {
+
+	cli_cmd = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show this help message",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		first_word := input[0]
+		cmd, ok := cli_cmd[first_word]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		cmd.callback()
 
 	}
 
@@ -38,16 +65,18 @@ func commandExit() error {
 	return nil
 }
 
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for _, cmd := range cli_cmd {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	return nil
+}
+
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
-}
-
-var cli_cmd = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
 }
