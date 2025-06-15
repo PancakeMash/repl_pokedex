2d571d9b9a34534a102a09fc15e1abf7db4492d9
PancakeMash <arpandevofficial@gmail.com>
2025-06-15 01:36:16 +0100

    C: Started adding in the repl command functionality. Will need to add/test the ability to read and callback the cmds.

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+
+	}
+
 }
 
 func cleanInput(text string) []string {
@@ -21,4 +30,24 @@ func cleanInput(text string) []string {
 	}
 
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cli_cmd = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
 }
